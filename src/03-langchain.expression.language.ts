abf6d92465e753516ebf8c84ed4c6eb154e8b429
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-26 00:12:07 +0100

    feat: langchain expression language

---
+++ src/03-langchain.expression.language.ts
@@ -0,0 +1,31 @@
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { ChatOpenAI } from '@langchain/openai';
+
+import('dotenv/config');
+
+export const langchainExpressionLanguage = async () => {
+  const model = new ChatOpenAI();
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `What are three good names for a company that makes {product}?`
+  );
+
+  const chain = prompt.pipe(model);
+
+  await chain.invoke({
+    product: 'colorful socks',
+  });
+
+  /**
+   *  AIMessage {
+        lc_serializable: true,
+        lc_kwargs: {
+          content: "1. Rainbow Socks Co.\n2. ChromaSock Inc.\n3. Colorful Feet Creations",
+          additional_kwargs: { function_call: undefined, tool_calls: undefined }
+        },
+        lc_namespace: [ "langchain_core", "messages" ],
+        content: "1. Rainbow Socks Co.\n2. ChromaSock Inc.\n3. Colorful Feet Creations",
+        name: undefined,
+        additional_kwargs: { function_call: undefined, tool_calls: undefined }
+      }
+   */
+};
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import { languageModel } from './01-language.model';
 import { promptTemplate } from './02-prompt.template';
+import { langchainExpressionLanguage } from './03-langchain.expression.language';
 
 (async () => {
-  //01-Language Model
+  //* 01-Language Model
   // await languageModel();
 
-  //02-Prompt Templates
-  await promptTemplate();
+  //* 02-Prompt Templates
+  // await promptTemplate();
+
+  //* 03-LangChain Expression Language (LCEL)
+  //await langchainExpressionLanguage()
+
+  //* 04-Output Parser
 })();
